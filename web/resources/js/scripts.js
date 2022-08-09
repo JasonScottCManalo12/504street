aa516ce17cf312113a67673b5892696a80da03aa
JasonScottCManalo12 <64289154+JasonScottCManalo12@users.noreply.github.com>
2022-08-09 21:21:57 +0800

    Update scripts.js

--- web/resources/js/scripts.js
+++ web/resources/js/scripts.js
@@ -1,2 +1,6 @@
+function getFooterCurrentDate() {
   const constFooterCurrentDate = new Date();
   document.getElementById("footerCurrentDate").innerHTML = constFooterCurrentDate.getFullYear();
+};
+
+getFooterCurrentDate();
